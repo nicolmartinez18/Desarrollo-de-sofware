4c9ed72ab21de4e01c129481d9c737e691cc8c14
Nicol Martinez <159297966+nicolmartinez18@users.noreply.github.com>
2024-05-15 01:05:17 -0400

    Add files via upload

---
+++ Algoritmo de calcular operaciones división y mod.py
@@ -0,0 +1,13 @@
+# Division
+num1 = 45
+num2 = 6
+num3 = num1 // num2  
+num4 = num1 / num2   
+
+print("División entera de", num1, "en", num2, "=", num3)
+print("División decimal de", num1, "en", num2, "=", num4)
+
+# Modulo
+num5 = num1%num2
+
+print("Mod de", num1, "en", num2, "=", num5)
